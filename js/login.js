8fcf04f08045aeeab712378c34f344a8d458a4dd
diegoperalta95 <diegooperalta95@gmail.com>
2020-08-08 19:54:39 -0300

    Commit Entrega 1
    Se añadió lo pedido para la entrega 1, teniendo en cuenta como estaba hecho el proyecto antes, para que esté todo a tono:
    
    Pautas individuales:
    + 2. Página de productos completa
    + 3 .Login hecho
    
    Pautas grupales:
    + 1. Validación del login
    
    Desafiate:
    + Hecho el login parcialmente (No hay logout, pero si login)

--- js/login.js
+++ js/login.js
@@ -1,6 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+    if (id_token != undefined || id_token != null){
+      window.location.href="home.html"
+    };    
+}
+
+document.addEventListener("DOMContentLoaded", function(e){  
+
+}
+);
