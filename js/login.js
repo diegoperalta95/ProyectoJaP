fa64ac9e4f05623e34195791bbecd66d23504271
diegoperalta95 <diegooperalta95@gmail.com>
2020-08-12 19:10:51 -0300

    Update Entrega 1 + Se agregó una verificación por JS además del required del HTML5 para la pauta grupal

--- js/login.js
+++ js/login.js
@@ -1,16 +1,16 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var id_token = googleUser.getAuthResponse().id_token;
-    if (id_token != undefined || id_token != null){
-      window.location.href="home.html"
-    };    
+  var profile = googleUser.getBasicProfile();
+  var id_token = googleUser.getAuthResponse().id_token;
+  if (id_token != undefined || id_token != null){
+    window.location.href="home.html"
+  };    
 }
 
-document.addEventListener("DOMContentLoaded", function(e){  
-
-}
-);
+var form = document.forms[0];
+form.addEventListener('submit', function(e){
+  usr = form[0].value;
+  pss = form[1].value;
+  if(usr == "" || usr == null || pss == "" || pss == null){
+    e.preventDefault();
+  }
+})
